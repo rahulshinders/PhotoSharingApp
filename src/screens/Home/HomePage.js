682a348e13d082e79febd1853cbb178ce0106944
Rahul Shinde <82095408+rahulshinders@users.noreply.github.com>
2024-08-08 16:55:58 +0530

    Update HomePage.js

--- src/screens/Home/HomePage.js
+++ src/screens/Home/HomePage.js
@@ -7,29 +7,6 @@ import Card from "../../components/card/Card";
 const url = "https://jsonplaceholder.typicode.com/users";
 
 const HomePage = () => {
-  const [input, setInput] = useState("");
-  const [results, setResults] = useState("");
-
-  const fetchData = (value) => {
-    fetch("") // empty fetch url
-      .then((response) => response.json())
-      .then((json) => {
-        const results = json.filter((user) => {
-          return (
-            value &&
-            user &&
-            user.component.title &&
-            user.component.title.toLowerCase().includes(value)
-          );
-        });
-        setResults(results);
-      });
-  };
-
-  const handleChange = (value) => {
-    setInput(value);
-    fetchData(value);
-  };
 
   return (
     <>
@@ -50,9 +27,6 @@ const HomePage = () => {
             <input
               type="text"
               placeholder="Search for anything..."
-              value={input}
-              onChange={(e) => handleChange(e.target.value)}
-              setResults={setResults}
             />
             {/* <Link className="submit" to={"/submit"}>
               <img
@@ -124,47 +98,3 @@ const HomePage = () => {
 
 export default HomePage;
 
-{
-  /* <Card
-          className="card-set"
-          image={require("../../assets/images/bottom-bar.png")}
-          title={"Bottom Navigation Bar"}
-          subtitle={"Rahul Shinde"}
-        />
-        <Card
-          className="card-set"
-          image={require("../../assets/images/bottom-nav.png")}
-          title={"Bottom Navigation Bar with Card"}
-          subtitle={"Rahul Shinde"}
-        />
-        <Card
-          className="card-set"
-          image={require("../../assets/images/button.png")}
-          title={"Animated Button"}
-          subtitle={"Rahul Shinde"}
-        />
-        <Card
-          className="card-set"
-          image={require("../../assets/images/card.png")}
-          title={"Beautiful Card"}
-          subtitle={"Rahul Shinde"}
-        />
-        <Card
-          className="card-set"
-          image={require("../../assets/images/dropdown.png")}
-          title={"DropDown Menu"}
-          subtitle={"Rahul Shinde"}
-        />
-        <Card
-          className="card-set"
-          image={require("../../assets/images/grid.png")}
-          title={"Grid Layout"}
-          subtitle={"Rahul Shinde"}
-        />
-        <Card
-          className="card-set"
-          image={require("../../assets/images/popper.png")}
-          title={"Popper Button"}
-          subtitle={"Rahul Shinde"}
-        /> */
-}
